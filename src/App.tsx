7a2f72dcea0120617f72958b8e13c1e3607026f2
Saeid Hosseinabadi <sdhosseinabadi@gmail.com>
2024-11-10 16:45:41 +0330

    addtheme

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import ParallaxComponent from './Parallax/ParallaxParallaxComponent';
-import MintApp from './MintApp/MintApp'; // Import the LaunchApp component
+import MintApp from './SignUp'; // Import the LaunchApp component
+import { StyledEngineProvider } from '@mui/material/styles';
 
 
 const App: React.FC = () => {
@@ -9,11 +10,12 @@ const App: React.FC = () => {
   return (
     <Router>
             <div>
-                
+            <StyledEngineProvider injectFirst>
                 <Routes>
                     <Route path="/" element={<ParallaxComponent />} />
                     <Route path="/mint" element={<MintApp />} />
                 </Routes>
+                </StyledEngineProvider>
             </div>
         </Router>
   );
