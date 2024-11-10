9f2af75eb653b34552037688619e171ef6390a3a
Saeid Hosseinabadi <sdhosseinabadi@gmail.com>
2024-11-11 00:20:07 +0330

    fix warning

--- src/MintApp/UploadToIPFS.tsx
+++ src/MintApp/UploadToIPFS.tsx
@@ -1,21 +1,6 @@
 // UploadToIPFS.tsx
 import React, { useState } from 'react';
 import { pinata } from '../utils/config'; // Ensure this is the correct path to your Pinata configuration
-import { createTheme, alpha, getContrastRatio } from '@mui/material/styles';
-
-const violetBase = '#7F00FF';
-const violetMain = alpha(violetBase, 0.7);
-
-const theme = createTheme({
-  palette: {
-    violet: {
-      main: violetMain,
-      light: alpha(violetBase, 0.5),
-      dark: alpha(violetBase, 0.9),
-      contrastText: getContrastRatio(violetMain, '#fff') > 4.5 ? '#fff' : '#111',
-    },
-  },
-});
 
 interface UploadToIPFSProps {
   base64Image: string | null;
